4e79614ce75a70257a130dece166501be05ba8e4
$ git config --global user.name "nadia.chaouch"$ git config --global user.email nadia.chaouch@docusign.com <nadia.chaouch@DSA004174.local>
2015-08-12 22:16:34 -0700

    new tts

--- hello.js
+++ hello.js
@@ -2,11 +2,10 @@
 @deploy
 */
 
-var speak = require('simple-tts');
+tts = require('./node-tts-api');
  
-// usage case 1 - writes the binary audio sample to the response stream (for returning it in an HTTP handler) 
-response.writeHead(200, {'Content-Type': 'audio/ogg'});
-speak('hello world', {format:'ogg', stream:response});
+example = "Hello World";
  
-// usage case 2 - creates the file /tmp/hello_world.mp3 in your OS (for caching or other purposes, the filename extension is automatically appended to the give filename) 
-speak('hello world', {format:'mp3', filename:'/tmp/hello_world'});
+tts.getSpeech(example, function(error, link) {
+  return console.log(link);
+});
